b2b694feb9850341d563efc0836c8922f33f5152
Alex Migdol and Zachary Richards <zachary.j.richards@gmail.com>
2016-06-01 20:25:11 -0700

    add remaining js for input form and re-iterating address

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var streetInput = $("input#street").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+
+
+    $(".name").text(nameInput);
+    $(".street").text(streetInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+
+    $("#thank_you").show();
+
+    event.preventDefault();
+});
 
 
 });
